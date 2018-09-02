7842159cb5248641423f92fec603911a295277ac
Matt0076 <mattyoung69.my@gmail.com>
2018-09-02 14:07:40 +1000

    Initial Commit 2/8/18
    The creation of the GitHub

---
+++ Happy Dance/Assets/_Scripts/EnableElVis.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableElVis : MonoBehaviour
+
+{
+
+    public float timer;
+
+    public GameObject objectToActivate1;
+    public GameObject objectToActivate2;
+    public GameObject objectToActivate3;
+    public GameObject objectToActivate4;
+
+    private void Start()
+    {
+
+        objectToActivate1.SetActive(false);
+        objectToActivate2.SetActive(false);
+        objectToActivate3.SetActive(false);
+        objectToActivate4.SetActive(false);
+        StartCoroutine(ActivationRoutine());
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        //Wait for 14 secs.
+        yield return new WaitForSeconds(timer);
+
+        //Turn My game object that is set to false(off) to True(on).
+        objectToActivate1.SetActive(true);
+        objectToActivate2.SetActive(true);
+        objectToActivate3.SetActive(true);
+        objectToActivate4.SetActive(true);
+
+        //Turn the Game Oject back off after 1 sec.
+        // yield return new WaitForSeconds(1);
+
+        //Game object will turn off
+        //objectToActivate.SetActive(false);
+    }
+}
