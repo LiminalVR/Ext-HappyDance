5abcafacb6ead2db36a2e79569630469b4842863
Matt0076 <mattyoung69.my@gmail.com>
2018-10-26 09:52:59 +1100

    DJ / Interactive level 26/10/18
    - Imported bongo audio
    - Set up a bongo prefab with audio and trigger collider
    - Set up a trigger script - when the user touches the bongo it triggers an audio clip
    - Each bongo has a different pitched audio

---
+++ Happy Dance/Assets/_Scripts/BongoTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BongoTrigger : MonoBehaviour
+
+
+{
+    public AudioClip bongo;    // Add your Audi Clip Here;
+
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = bongo;
+    }
+
+    void OnTriggerEnter()  //Plays Sound Whenever trigger detected
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+
+}
