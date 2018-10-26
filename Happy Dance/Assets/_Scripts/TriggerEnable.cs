9db71d1d9b37f059452a8efc88c7bd92d38ac0e9
Matt0076 <mattyoung69.my@gmail.com>
2018-10-26 13:39:45 +1100

    DJ Level (003) 26/10/18
    - Setup buttons to activate dancers - one each (2)
    - Setup on trigger enter script to acrivate dancer , their individual beams and spotlights (2)
    - setup the beams ans spaceship
    - To be tested when i recieve my Gear VR setup

---
+++ Happy Dance/Assets/_Scripts/TriggerEnable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnable : MonoBehaviour
+{
+
+    public float timer;
+
+    public GameObject hand;
+
+    public GameObject objectToActivate;
+    public GameObject objectToActivate2;
+    public GameObject objectToActivate3;
+
+    private void Start()
+{
+
+
+        objectToActivate.SetActive(false);
+        objectToActivate2.SetActive(false);
+        objectToActivate3.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider hand)
+
+
+    {
+
+        StartCoroutine(ActivationRoutine());
+
+    }
+
+    private IEnumerator ActivationRoutine()
+{
+    //Wait for 14 secs.
+    yield return new WaitForSeconds(timer);
+
+        //Turn My game object that is set to false(off) to True(on).
+        objectToActivate.SetActive(true);
+        objectToActivate2.SetActive(true);
+        objectToActivate3.SetActive(true);
+
+        //Turn the Game Oject back off after 1 sec.
+        // yield return new WaitForSeconds(1);
+
+        //Game object will turn off
+        //objectToActivate.SetActive(false);
+    }
+    }
