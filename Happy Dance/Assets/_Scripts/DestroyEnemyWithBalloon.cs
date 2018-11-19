9388ced626f6684e28b87c8831dfb118c55381d5
Matt0076 <mattyoung69.my@gmail.com>
2018-11-19 17:36:38 +1100

    Tester
    Testing interactive features in test level

---
+++ Happy Dance/Assets/_Scripts/DestroyEnemyWithBalloon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemyWithBalloon : MonoBehaviour 
+
+{
+
+	public AudioClip sound;
+	AudioSource audiosource;
+
+
+	// Use this for initialization
+	void Start () 
+
+	{
+
+		audiosource = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+	{
+
+
+
+	}
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Destroy") 
+		
+		{
+
+			audiosource.PlayOneShot (sound, 0.1f);
+
+			Destroy (other.gameObject);
+		}
+
+	}
+}
+
