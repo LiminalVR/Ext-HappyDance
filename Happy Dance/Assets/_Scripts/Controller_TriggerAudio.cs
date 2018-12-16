e5fdd550e2867602fb871e1691e1174e2e2163b1
Matt0076 <mattyoung69.my@gmail.com>
2018-12-16 16:02:07 +1100

    Happy Dance 13/12/18
    Stripping back to dance mode

---
+++ Happy Dance/Assets/_Scripts/Controller_TriggerAudio.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Controller_TriggerAudio : MonoBehaviour
+
+
+
+{   
+
+	public GameObject Camera;
+
+	public GameObject controller;
+
+	//public float DelayAudio;
+
+	public AudioClip[] audioArray;
+
+	AudioSource audioSource;
+
+	AudioClip currentAudio;
+
+	int index;
+
+
+	//private IEnumerator coroutine;
+
+
+
+	void Start()
+
+	{
+
+		
+		audioSource = Camera.transform.Find("EGO_ControllerTrigger").GetComponent<AudioSource> ();
+	
+
+		index = Random.Range (0, audioArray.Length);
+		currentAudio = audioArray[index];
+		print (currentAudio.name);
+
+		//coroutine = PlayAudioWithDelay (3.0f);
+
+	}
+
+
+	void OnTriggerExit(Collider controller)
+
+	{
+		//StartCoroutine (PlayAudioWithDelay(1));
+		audioSource.PlayOneShot(currentAudio);
+
+	}
+		
+
+	//private IEnumerator PlayAudioWithDelay (float Delay)
+
+	//{
+		//if (currentAudio == null)
+			//yield break;
+		//yield return new WaitForSeconds(Delay);
+
+		//audioSource.PlayOneShot(currentAudio);
+
+	//}
+
+
+
+
+}
