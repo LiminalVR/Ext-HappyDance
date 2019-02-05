849236f5a51309fdf30bb6deed251e9affdd01f3
Matt0076 <mattyoung69.my@gmail.com>
2019-02-05 11:58:17 +1100

    Audio work 5/2/19
    - Fined tuned beam audio
    - Trying to work out the controller trigger audio

--- Happy Dance/Assets/_Scripts/Controller_TriggerAudio.cs
+++ Happy Dance/Assets/_Scripts/Controller_TriggerAudio.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 
 public class Controller_TriggerAudio : MonoBehaviour
-
-
-
 {   
 
 	public GameObject Camera;
@@ -25,6 +22,8 @@ public class Controller_TriggerAudio : MonoBehaviour
 	int index;
 
 
+	private bool canPlay = true;
+
 	//private IEnumerator coroutine;
 
 
@@ -33,9 +32,9 @@ public class Controller_TriggerAudio : MonoBehaviour
 
 	{
 
-		
-		audioSource = Camera.transform.Find("EGO_ControllerTrigger").GetComponent<AudioSource> ();
-	
+
+		//audioSource = Camera.transform.Find("EGO_ControllerTrigger").GetComponent<AudioSource> ();
+
 
 		index = Random.Range (0, audioArray.Length);
 		currentAudio = audioArray[index];
@@ -45,24 +44,53 @@ public class Controller_TriggerAudio : MonoBehaviour
 
 	}
 
-
-	void OnTriggerExit(Collider controller)
-
+	private void Update()
 	{
-		//StartCoroutine (PlayAudioWithDelay(1));
-		audioSource.PlayOneShot(currentAudio);
+		var forward = transform.TransformDirection(Vector3.forward);
+		var toOther = Vector3.up;
 
+		//gets the angle between the controller's forward vector and the upwards vector (0,1,0) and returns it as a value between -1 and 1.
+		//-1 means you're pointing directly away from the target and 1 means you're pointing directly at it
+		var dotProduct = Vector3.Dot(forward, toOther);
+
+		//fires the sound and makes it so that canPlay=false preventing it from firing again
+		if (dotProduct > 0.95f && canPlay)
+		{
+			canPlay = false;
+
+			currentAudio = audioArray[Random.Range(0, audioArray.Length)];
+
+			//audioSource.PlayOneShot(currentAudio);
+
+			AudioSource.PlayClipAtPoint(currentAudio, new Vector3(5, 1, 2));
+
+
+		}
+		else if (dotProduct < 0.35f && canPlay == false)
+		{
+			canPlay = true;
+		}
 	}
-		
+
+
+
+	//void OnTriggerExit(Collider controller)
+
+	//{
+		//StartCoroutine (PlayAudioWithDelay(1));
+
+
+	//}
+
 
 	//private IEnumerator PlayAudioWithDelay (float Delay)
 
 	//{
-		//if (currentAudio == null)
-			//yield break;
-		//yield return new WaitForSeconds(Delay);
+	//if (currentAudio == null)
+	//yield break;
+	//yield return new WaitForSeconds(Delay);
 
-		//audioSource.PlayOneShot(currentAudio);
+	//audioSource.PlayOneShot(currentAudio);
 
 	//}
 
