f10de8560d168e5a924bc3df2be68a0abc3ccc28
Matt0076 <mattyoung69.my@gmail.com>
2019-03-19 10:10:33 +1100

    Audio and End screen 19/3/19
    - Sorted the positive audio triggers
    - Added end screen on a timer

---
+++ Happy Dance/Assets/_Scripts/Audio_Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Timer : MonoBehaviour 
+
+{
+
+	public AudioClip[] vocalAudio;
+	AudioSource voices;
+
+	//Every 0.5 seconds(Change this to your needs)
+	const float coolDownTime = 0.5f;
+	float startingTime = 0f;
+
+	void Start()
+	{
+		voices = GetComponent<AudioSource>();
+		startingTime = Time.time;
+
+		//We have. Now reset timer
+		startingTime = Time.time;
+
+		playAudio();
+
+	}
+
+	//void Update()
+	//{
+		
+		//if (Input.GetKey(KeyCode.Space))
+		//{
+			//Check if we have reached the cool down timer
+			//if (Time.time > startingTime + coolDownTime)
+			//{
+				//We have. Now reset timer
+				//startingTime = Time.time;
+
+				//playAudio();
+			//}
+		//}
+	//}
+
+	void playAudio()
+	{
+		voices.PlayOneShot(vocalAudio[UnityEngine.Random.Range(0, vocalAudio.Length)]);
+		UnityEngine.Debug.Log("Shot");
+	}
+
+}
---
+++ Happy Dance/Assets/_Scripts/SceneChange_Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange_Timer : MonoBehaviour 
+
+{
+	bool loadingStarted = false;
+	float secondsLeft = 0;
+
+	void Start()
+	{
+		StartCoroutine(DelayLoadLevel(320));
+	}
+
+	IEnumerator DelayLoadLevel(float seconds)
+	{
+		secondsLeft = seconds;
+		loadingStarted = true;
+		do
+		{
+			yield return new WaitForSeconds(1);
+		} while (--secondsLeft > 0);
+
+		Application.LoadLevel("Outro");
+	}
+
+	void OnGUI()
+	{
+		if (loadingStarted)
+			GUI.Label(new Rect(0, 0, 100, 20), secondsLeft.ToString());
+	}
+}
