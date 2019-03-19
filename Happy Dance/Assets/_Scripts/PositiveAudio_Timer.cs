43e1506b5441f95e796c5a2da9967485adf00c64
Matt0076 <mattyoung69.my@gmail.com>
2019-03-20 05:44:27 +1100

    audio / font 20/3/19
    - Set up the menu and outro screen fonts
    - Set up audio  tiggered off by rime - need to change the test audio to the real voice tracks

---
+++ Happy Dance/Assets/_Scripts/PositiveAudio_Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PositiveAudio_Timer : MonoBehaviour
+{
+
+	public float timer;
+
+	//public GameObject objectToActivate;
+
+	public AudioClip beam;
+	AudioSource audioSource;
+
+	private void Start()
+	{
+
+		//objectToActivate.SetActive(false);
+		StartCoroutine(ActivationRoutine());
+
+		//audioSource = mainCamera.Find.GetComponent<AudioSource>();
+
+		//AudioSource.PlayClipAtPoint(beam, new Vector3(5, 1, 2));
+
+	}
+
+	public IEnumerator ActivationRoutine()
+	{
+		//Wait for 14 secs.
+		yield return new WaitForSeconds(timer);
+
+		//Turn My game object that is set to false(off) to True(on).
+		//objectToActivate.SetActive(true);
+
+		//audioSource.PlayOneShot(beam, 1.0f);
+
+		AudioSource.PlayClipAtPoint(beam, new Vector3(5, 1, 2));
+
+		//Turn the Game Oject back off after 1 sec.
+		// yield return new WaitForSeconds(1);
+
+		//Game object will turn off
+		//objectToActivate.SetActive(false);
+	}
+}
