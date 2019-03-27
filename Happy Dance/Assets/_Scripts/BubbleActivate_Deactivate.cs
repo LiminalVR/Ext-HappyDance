01605a992800d9035de3423d6ec6bfcdf68fe537
Matt0076 <mattyoung69.my@gmail.com>
2019-03-27 11:25:32 +1100

    Main scene 27/3/19
    - Timed the speech bubbles, audio and text

--- Happy Dance/Assets/_Scripts/BubbleActivate_Deactivate.cs
+++ Happy Dance/Assets/_Scripts/BubbleActivate_Deactivate.cs
@@ -25,7 +25,7 @@ public class BubbleActivate_Deactivate : MonoBehaviour
 			objectToActivate.SetActive(true);
 
 			//Turn the Game Oject back off after 1 sec.
-			yield return new WaitForSeconds(2.0f);
+			yield return new WaitForSeconds(3.0F);
 
 			//Game object will turn off
 			objectToActivate.SetActive(false);
