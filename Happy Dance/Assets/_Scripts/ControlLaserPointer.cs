5e37ca1e492116bab9d5096e3fdfccef3b99bbdd
MichaelV <michael@liminalvr.com>
2019-06-27 11:34:20 +1000

    Disable controller laser pointer on start.

---
+++ Happy Dance/Assets/_Scripts/ControlLaserPointer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using Liminal.SDK.VR.Pointers;
+
+/// <summary>
+/// ControlLaserPointer controls whether the laser pointer visual is displayed for the player.
+/// </summary>
+public class ControlLaserPointer 
+    : MonoBehaviour
+{
+    public bool Enabled;
+    void Start()
+    {
+        var pointer = GetComponentInChildren<LaserPointerVisual>();
+        var lineRend = GetComponentInChildren<LineRenderer>();
+
+        pointer.enabled = Enabled;
+        lineRend.enabled = Enabled;
+    }
+}
