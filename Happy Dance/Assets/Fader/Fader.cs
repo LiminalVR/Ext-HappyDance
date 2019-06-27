c6258e65bdf6552d948d6ac147c0a33101e875f5
MichaelV <michael@liminalvr.com>
2019-06-27 16:34:27 +1000

    Add an inverted sphere with material and animation to fade in and shrink over time so that we can end the experience better.

---
+++ Happy Dance/Assets/Fader/Fader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    public Color Color;
+    public Material FaderMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FaderMaterial.SetColor("_Tint",Color);
+    }
+}
