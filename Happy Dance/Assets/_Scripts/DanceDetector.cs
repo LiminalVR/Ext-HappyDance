a6545a3bb2d09de4ffe7e943f6db92f7d2654ab5
MichaelV <michael@liminalvr.com>
2019-06-27 11:34:05 +1000

    Rename Praise class to Speech (and change fields as needed). Rename PraiseController to DanceDetector for clarity. Add code to DanceDetector to tell when the player isn't dancing and encourage them as needed.

---
+++ Happy Dance/Assets/_Scripts/DanceDetector.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// DanceDetector measures how much the player is moving their controller around and then plays a random praise track when it exceeds a certain value.
+/// DanceDetector then waits a random amount of time before being able to play another praise track.
+/// </summary>
+public class DanceDetector 
+    : MonoBehaviour
+{
+    public Transform HandTransform;
+    public float CommentActivationMovementThreshold;
+    public float TimeBeforeEncouragement;
+    [Space]
+    public List<Speech> PlayerPraiseOptions;
+    public List<Speech> PlayerEncouragementOptions;
+    public List<SpeechBubble> SpeechBubbles;
+    [Space]
+    public float BaseTalkCooldown;
+    public Vector2 CooldownRange;
+    public bool Active;
+
+    private Vector3 _cachedPos;
+    private Coroutine _talkRoutine;
+    private float _timeSinceGrossMovement;
+
+    void Start()
+    {
+        Assert.IsNotNull(HandTransform, "HandTransform must not be null!");
+        _cachedPos = HandTransform.position;
+        Active = true;
+    }
+
+    private void Update()
+    {
+        if (!Active)
+            return;
+
+        var handDelta = HandTransform.position - _cachedPos;
+
+        if (_talkRoutine == null)
+        {
+            _timeSinceGrossMovement += Time.deltaTime;
+
+            if (handDelta.magnitude > CommentActivationMovementThreshold)
+            {
+                _talkRoutine = StartCoroutine(TalkRoutine(PlayerPraiseOptions));
+                _timeSinceGrossMovement = 0;
+            }
+
+            if (_timeSinceGrossMovement > TimeBeforeEncouragement)
+            {
+                _talkRoutine = StartCoroutine(TalkRoutine(PlayerEncouragementOptions));
+                    _timeSinceGrossMovement = 0;
+            }
+        }
+
+        _cachedPos = HandTransform.position;
+    }
+
+    private IEnumerator TalkRoutine(List<Speech> talkList)
+    {
+        var praise = talkList[Random.Range(0, talkList.Count)];
+        var praiseDisplay = SpeechBubbles[Random.Range(0, SpeechBubbles.Count)];
+
+        praiseDisplay.DisplayGameObject.SetActive(true);
+        praiseDisplay.SetSpeechBubble(praise);
+
+        yield return new WaitForSeconds(praise.SpeechAudioClip.length + 0.1f);
+        praiseDisplay.DisplayGameObject.SetActive(false);
+
+        yield return new WaitForSeconds(BaseTalkCooldown + Random.Range(CooldownRange.x, CooldownRange.y));
+
+        _talkRoutine = null;
+
+    }
+}
--- Happy Dance/Assets/_Scripts/SpeechBubble.cs
+++ Happy Dance/Assets/_Scripts/SpeechBubble.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// SpeechBubble is used to store the text component and audiosource of text display object. It also contains a reference to the object itself so <see cref="PraiseController"/> can activate the object.
+/// SpeechBubble is used to store the text component and audiosource of text display object. It also contains a reference to the object itself so <see cref="DanceDetector"/> can activate the object.
 /// It also contains a method to set the display text and play the praise's audioclip.
 /// </summary>
 [Serializable]
@@ -14,10 +14,10 @@ public class SpeechBubble
     public Text DisplayText;
     public AudioSource PraiseSource;
 
-    public void SetSpeechBubble(Praise praiseToDisplay)
+    public void SetSpeechBubble(Speech speechToDisplay)
     {
-        DisplayText.text = praiseToDisplay.PraiseText;
-        PraiseSource.clip = praiseToDisplay.PraiseAudioClip;
+        DisplayText.text = speechToDisplay.SpeechText;
+        PraiseSource.clip = speechToDisplay.SpeechAudioClip;
         PraiseSource.Play();
     }
 }
