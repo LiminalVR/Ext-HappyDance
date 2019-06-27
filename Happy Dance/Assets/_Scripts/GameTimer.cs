43b24724bfbee7138586c4cade33f0deefc0fce9
MichaelV <michael@liminalvr.com>
2019-06-27 16:35:19 +1000

    Add ending timeline to the scene to end the experience once activated by Game Timer.

--- Happy Dance/Assets/_Scripts/GameTimer.cs
+++ Happy Dance/Assets/_Scripts/GameTimer.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Playables;
 
 using Liminal.Core.Fader;
 using Liminal.SDK.Core;
 
+/// <summary>
+/// GameTimer is sued to control the ending of the happy dance experience.
+/// </summary>
 public class GameTimer 
     : MonoBehaviour
 {
-    public float SecondsLeft = 0;
+    public float SecondsLeft;
+    public DanceDetector PlayerDanceDetector;
+    public PlayableDirector Director;
 
     void Start()
-	{
+    {
+        Assert.IsNotNull(PlayerDanceDetector, "PlayerDanceDetector must not be null");
 		StartCoroutine(TimerCoro());
 	}
 
@@ -22,6 +30,13 @@ public class GameTimer
             SecondsLeft -= Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+
+        PlayerDanceDetector.Active = false;
+
+        yield return new WaitForSeconds(5f);
+        Director.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds((float) Director.duration + 0.1f);
 
         ScreenFader.Instance.FadeToBlack(2);
         yield return new WaitForSeconds(2f);
