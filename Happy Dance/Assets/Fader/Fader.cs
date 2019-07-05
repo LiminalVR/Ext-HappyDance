af66bc8c11a5916527fd75c6c0b7a3c1a985bd6d
MichaelV <michael@liminalvr.com>
2019-07-05 14:16:20 +1000

    Refactor Fader script and add assert.

--- Happy Dance/Assets/Fader/Fader.cs
+++ Happy Dance/Assets/Fader/Fader.cs
@@ -1,21 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Assertions;
 
-public class Fader : MonoBehaviour
+/// <summary>
+/// Fader is used to controller the color of a target material. It is driven by an animation clip.
+/// </summary>
+public class Fader 
+    : MonoBehaviour
 {
     public Color Color;
     public Material FaderMaterial;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnValidate()
     {
-        
+        Assert.IsNotNull(FaderMaterial, "FaderMaterial must not be null.");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        FaderMaterial.SetColor("_Tint",Color);
+        FaderMaterial.SetColor("_Tint", Color);
     }
 }
